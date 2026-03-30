c853816dcac009f2dec7fbd7a84bdf7bcea0af41
Tinomudaishe Tererai Manyeruke <tinomanyeruke@gmail.com>
2026-03-30 16:25:19 +0200

    Update customizesmoothie.py

--- customizesmoothie.py
+++ customizesmoothie.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import pandas
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Example Streamlit App :cup_with_straw: {st.__version__}")
